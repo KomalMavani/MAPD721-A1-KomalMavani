f2ac0b1ac4d851aa02fb5c8559b699cc6ec1e2e7
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2025-02-01 11:51:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mapd721_a1_komalmavani/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mapd721_a1_komalmavani
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mapd721_a1_komalmavani.ui.theme.MAPD721A1KomalMavaniTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MAPD721A1KomalMavaniTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MAPD721A1KomalMavaniTheme {
+        Greeting("Android")
+    }
+}
