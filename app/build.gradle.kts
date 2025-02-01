7d5763e1e136564d8806e886fb54288e350a9e6c
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2025-02-01 12:10:44 -0500

    Change compileSDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mapd721_a1_komalmavani"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mapd721_a1_komalmavani"
